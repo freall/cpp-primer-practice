b6c13abdbb8d883013e31ead30bba5d2eb2e309b
freall <freall@126.com>
2015-06-22 11:34:34 +0800

    just try edit by Xcode

--- 01/1-2.cpp
+++ 01/1-2.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
+#include <string>
 using std::cout;
 using std::endl;
 int main()
 {
     cout << "Hello, world" << endl;
-
+    std::cout << std::string("hello world") << endl;
     return -1;
 }
